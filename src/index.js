7966b591efb48e58d863cad41b4de1da1bb4e621
moh-x <mo.xodeeq@gmail.com>
2022-04-05 16:39:54 +0100

    Task completion

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ http
 	.createServer((req, res) => {
 		console.log(`${req.method} request for ${req.url}`);
 		if (req.url === "/") {
-			fs.readFile("./home.html", "UTF-8", function (err, html) {
+			fs.readFile("./home.html", "UTF-8", function (err, content) {
+				if (err) console.log(err);
 				res.writeHead(200, { "Content-Type": "text/html" });
-				res.end(html);
+				res.end(content, "utf-8");
 			});
 		} else if (req.url === "/contact") {
-			fs.readFile("./contact.html", "UTF-8", function (err, html) {
+			fs.readFile("./contact.html", "UTF-8", function (err, content) {
+				if (err) console.log(err);
 				res.writeHead(200, { "Content-Type": "text/html" });
-				res.end(html);
+				res.end(content, "utf-8");
 			});
 		} else if (req.url === "/home") {
 			res.writeHead(302, {
@@ -22,9 +24,10 @@ http
 			});
 			res.end();
 		} else if (req.url === "/about") {
-			fs.readFile("./about.html", "UTF-8", function (err, html) {
+			fs.readFile("./about.html", "UTF-8", function (err, content) {
+				if (err) console.log(err);
 				res.writeHead(200, { "Content-Type": "text/html" });
-				res.end(html);
+				res.end(content, "utf-8");
 			});
 		}
 	})
